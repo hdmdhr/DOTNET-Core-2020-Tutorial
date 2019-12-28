cc52f641cc7ca142beec1b867768bf5fae55a6ae
DongMing <hdmdhr@gmail.com>
2019-12-28 15:38:40 -0700

    18-19. Create Model classes (Employee, EmployeeRepository). | Register DI Container to use MockEmployeeRePository in ConfigureServices in Startup.cs (.AddSingleton<>).

--- empty_project/Controllers/HomeController.cs
+++ empty_project/Controllers/HomeController.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using empty_project.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace empty_project.Controllers
 {
     public class HomeController : Controller
     {
+        private IEmployeeRepository _employeeRepo;
+
+        public HomeController(IEmployeeRepository employeeRepo)
+        {
+            _employeeRepo = employeeRepo;
+        }
+
         public IActionResult Index()
         {
-            return Json(new {id=1, name="Daniel Hu"});
+            return Json(_employeeRepo.GetEmployee(1));
         }
     }
 }
---
+++ empty_project/Models/MockEmployeeRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empty_project.Models
+{
+    public class MockEmployeeRepository: IEmployeeRepository
+    {
+        private List<Employee> _employeeList;
+
+        public MockEmployeeRepository()
+        {
+            _employeeList = new List<Employee>()
+            {
+                new Employee(){Id = 1, Department = "Sales", Email = "1@2.com", Name = "Jeff Fired"},
+                new Employee(){Id = 2, Department = "Admin", Email = "1@3.com", Name = "Vince Walkman"},
+                new Employee(){Id = 3, Department = "IT", Email = "1@4.com", Name = "Daniel Bravo"}
+            };
+        }
+
+        public Employee GetEmployee(int Id)
+        {
+            return _employeeList.FirstOrDefault(e => e.Id == Id);
+        }
+    }
+}
