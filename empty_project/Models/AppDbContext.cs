c5917b6f089672cd963a5dc599b9f0908d4bef00
Dongming Hu <hdmdhr@gmail.com>
2020-01-10 13:46:48 -0700

    47. Setup DbContext and DbSet<TEntity>.

---
+++ empty_project/Models/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empty_project.Models
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+
+        // for every custom class in the app, need one property
+        // LINQ query against DbSet<TEntity> will be translated into SQL query
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
