6fad75e609ea9019c42de12766a12810b4b24693
Dongming Hu <hdmdhr@gmail.com>
2020-01-17 15:37:34 -0700

    54.Upload Multiple Files: use multiple tag on file upload <input>. | Modify JavaScript in Create View to handle multiple file uploads. | Modify EmployeeCreateViewModel, Create Action implementations.

--- empty_project/Controllers/HomeController.cs
+++ empty_project/Controllers/HomeController.cs
@@ -51,21 +51,24 @@ namespace empty_project.Controllers
             if (ModelState.IsValid)
             {
                 string uniqueFileName = null;
-                if (vm.Photo != null)
+                if (vm.Photos != null && vm.Photos.Count > 0)
                 {
-                    // generate unique file path
-                    string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
-                    uniqueFileName = $"{Guid.NewGuid().ToString()}_{vm.Photo.FileName}";
-                    string filePath = Path.Combine(uploadsFolder, uniqueFileName);
-                    // copy uploaded photo to specified file path
-                    vm.Photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                    foreach (var photo in vm.Photos)
+                    {
+                        // generate unique file path
+                        string uploadsFolder = Path.Combine(_hostingEnvironment.WebRootPath, "images");
+                        uniqueFileName = $"{Guid.NewGuid().ToString()}_{photo.FileName}";
+                        string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                        // copy uploaded photo to specified file path
+                        photo.CopyTo(new FileStream(filePath, FileMode.Create));
+                    }
                 }
                 var newEmployee = _employeeRepo.Add(new Employee
                 {
                     Name = vm.Name,
                     Email = vm.Email,
                     Department = vm.Department,
-                    PhotoPath = uniqueFileName
+                    PhotoPath = uniqueFileName  // use the last image's path
                 });
                 return RedirectToAction("Details", new {id = newEmployee.Id});
             }
