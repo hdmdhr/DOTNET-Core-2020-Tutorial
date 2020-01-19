6f378637f4826c7f8dfaed5449db52ff9001ad54
DongMing <hdmdhr@gmail.com>
2020-01-18 19:32:45 -0700

    66. Add Register View: create RegisterViewModel, create Register View, add register link to nav bar, use .ml-auto to move it to right.

---
+++ empty_project/Controllers/AccountController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace empty_project.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
