1f4b6850980c0d82a2a154d774b076fb3da2ac7b
DongMing <hdmdhr@gmail.com>
2020-01-18 20:25:12 -0700

    67. UserManager & SignInManager: inject UserManager & SignInManager into AccountController, use .CreateAsync() to create a user, if .Succeeded use .SignInAsync() to sign in; if not .Succeeded, .AddModelError() to ModelState and return Register View again.

--- empty_project/Controllers/AccountController.cs
+++ empty_project/Controllers/AccountController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using empty_project.ViewModels;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 
@@ -9,10 +11,43 @@ namespace empty_project.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser { UserName = vm.Email, Email = vm.Email };
+                var result = await _userManager.CreateAsync(user, vm.Password);
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(vm);
         }
     }
 }
