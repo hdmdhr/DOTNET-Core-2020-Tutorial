34b246216a5139222aa3d8cb560216f6aeed68f8
DongMing <hdmdhr@gmail.com>
2020-01-19 18:51:41 -0700

    78. Create Role: create AdministrationController, use ctor to inject RoleManager instance. | CreateRoleViewModel, CreateRole View, Get & Post CreateRole Actions.

---
+++ empty_project/Controllers/AdministrationController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using empty_project.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace empty_project.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministrationController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var role = new IdentityRole(vm.RoleName);
+                var result = await _roleManager.CreateAsync(role);
+                if (result.Succeeded)
+                    return RedirectToAction("Index", "Home");
+
+                foreach (var error in result.Errors)
+                    ModelState.AddModelError("", error.Description);
+            }
+
+            return View(vm);
+        }
+    }
+}
---
+++ empty_project/ViewModels/CreateRoleViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace empty_project.ViewModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        [DisplayName("New Role Name")]
+        public string RoleName { get; set; }
+    }
+}
