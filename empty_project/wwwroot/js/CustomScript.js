ec9b55ab274c84690ee438d3b9ddad9a8537e6f1
Dongming Hu <hdmdhr@gmail.com>
2020-01-21 10:33:31 -0700

    87. Delete Confirmation: use JavaScript to achieve show/hide inline confirmation. | Remove href tag to disable scroll to top behavior, add custom style class to keep bootstrap styles the same.

--- empty_project/wwwroot/js/CustomScript.js
+++ empty_project/wwwroot/js/CustomScript.js
@@ -1,3 +1,18 @@
 ﻿var h2 = document.createElement("h2");
 h2.innerHTML = "Hello from CustomScript";
 document.body.appendChild(h2);
+
+
+function confirmDelete(userId, isDeleteClicked) {
+    var deleteId = '#delete_' + userId;
+    var confirmDeleteId = '#confirmDelete_' + userId;
+
+    if (isDeleteClicked) {
+        console.log(deleteId);
+        $(deleteId).hide();
+        $(confirmDeleteId).show();
+    } else {
+        $(deleteId).show();
+        $(confirmDeleteId).hide();
+    }
+}
